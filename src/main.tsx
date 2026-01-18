77723cb1e6c16565179c72b68385a83925fc0694
Scott Peabody <dev.peabody@outlook.com>
2026-01-17 19:55:35 -0500

    fix: import MDXEditor styles in main.tsx for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import App from './App'
 import { ThemeProvider } from './context/ThemeContext'
 import { mdxComponents } from './components/mdx'
 import './index.css'
+// Import MDXEditor styles here (not in lazy-loaded component) to ensure they're always available
+import '@mdxeditor/editor/style.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
